b942719601c7a42099614c5e9f65931b85b06449
Hexagon <hexagon@56k.guru>
2023-09-29 12:51:57 +0200

    Update

--- docs/_config.ts
+++ docs/_config.ts
@@ -3,7 +3,7 @@ import lumocs from "lumocs/mod.ts";
 
 const site = lume({
   src: "src",
-  location: new URL("https://hexagon.github.io/lumocs/"),
+  location: new URL("https://lumocs.56k.guru"),
 });
 
 site.use(lumocs());
